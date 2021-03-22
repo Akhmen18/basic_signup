cfe41b673ce06089e468a4850ac62084f0ff4be7
Akhil Menon <akh180599@gmail.com>
2021-03-22 14:27:25 +0530

    Fix up passing info (2)

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   final String name, email;
 
-  HomePage({this.name, this.email});
+  HomePage({Key key, @required this.name, this.email});
 
   @override
   Widget build(BuildContext context) {
